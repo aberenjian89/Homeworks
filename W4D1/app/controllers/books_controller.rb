e1f004a99170221d106131f68743524a822c71b5
Ali Berenjian <Ali.rberenjian@gmail.com>
2017-12-27 20:38:42 -0800

    Complete Homework

---
+++ W4D1/app/controllers/books_controller.rb
@@ -0,0 +1,33 @@
+class BooksController < ApplicationController
+  def index
+    # your code here
+    @books=Book.all
+  end
+
+  def new
+    # your code here
+  end
+
+  def create
+    # your code here
+    book=Book.new(book_params)
+    if book.save
+      redirect_to books_path
+    else
+      render json: 'Error'
+    end
+  end
+
+  def destroy
+    # your code here
+    # find Book from Database and Deleted
+    book=Book.find(params[:id])
+    book.destroy
+    redirect_to books_url
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :author)
+  end
+end
